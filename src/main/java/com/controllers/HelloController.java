8d840b16f503361f3b294e61b2238aebb70d2e14
dima_z <bengorde80@gmail.com>
2017-03-18 16:19:36 +0200

    project structure created

---
+++ src/main/java/com/controllers/HelloController.java
@@ -0,0 +1,29 @@
+package com.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.io.IOException;
+
+/**
+ * Created by zvazhiidmytro on 18.03.17.
+ */
+@Controller
+@RequestMapping("/")
+public class HelloController {
+    @RequestMapping(method = RequestMethod.GET)
+    public String printWelcome(ModelMap model) {
+        model.addAttribute("message", "Hello world!");
+        return "hello";
+    }
+
+    @Autowired
+    private ApplicationContext applicationContext;
+
+
+}
---
+++ src/main/webapp/WEB-INF/pages/hello.jsp
@@ -0,0 +1,11 @@
+<%@ taglib prefix="form" uri="http://www.springframework.org/tags/form" %>
+<html>
+<body>
+<h1>Hello world</h1>
+
+<%--<form method="post" action="/search">--%>
+    <%--<input name="input" type="text" value=""/>--%>
+    <%--<button id="ex" name="ex" type="submit">Execute</button>--%>
+<%--</form>--%>
+</body>
+</html>
