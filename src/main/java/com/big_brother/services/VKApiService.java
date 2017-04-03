4f65027075d9eb98063d7d30485dc7bd81d5a9ca
fa2m-alex <alex.fa2m@yandex.ua>
2017-04-03 20:04:09 +0300

    VKApi Service updated

--- src/main/java/com/big_brother/services/VKApiService.java
+++ src/main/java/com/big_brother/services/VKApiService.java
@@ -1,7 +1,10 @@
 package com.big_brother.services;
 
+import com.big_brother.dto.User;
+import com.big_brother.dto.UserResponse;
 import org.json.JSONArray;
 import org.json.JSONObject;
+import org.springframework.web.client.RestTemplate;
 
 import java.io.*;
 import java.net.MalformedURLException;
@@ -13,49 +16,12 @@ import java.net.URLEncoder;
  * Created by Alex on 03.04.2017.
  */
 public class VKApiService {
-    private static final String URL = "https://api.vk.com/method/users.get";
-    private static final String CHARSET = "UTF-8";
-
-    public VKApiService(){}
-
     public static boolean getVKUserStatus(String vkId){
+        RestTemplate restTemplate = new RestTemplate();
+        User user = restTemplate.getForObject("https://api.vk.com/method/users.get?user_ids=" +
+                vkId + "&fields=online", User.class);
 
-        String query = null;
-        URLConnection connection = null;
-        InputStream response = null;
-        BufferedReader br = null;
-        JSONObject recvObj = null;
-
-        try {
-            query = String.format("user_ids=%s&fields=online", URLEncoder.encode(vkId, CHARSET));
-            connection = new URL(URL + "?" + query).openConnection();
-            connection.setRequestProperty("Accept-Charset", CHARSET);
-            response = connection.getInputStream();
-
-            br = new BufferedReader(new InputStreamReader(response));
-
-            String line;
-            StringBuilder sb = new StringBuilder();
-            while ((line = br.readLine()) != null) {
-                sb.append(line);
-            }
-
-            recvObj = new JSONObject(sb.toString());
-
-            response.close();
-            br.close();
-
-        } catch (UnsupportedEncodingException e) {
-            e.printStackTrace();
-        } catch (MalformedURLException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-        JSONArray root = recvObj.getJSONArray("response");
-        JSONObject user = root.getJSONObject(0);
-
-        return user.getInt("online") != 0;
+        UserResponse response = (UserResponse) user.getResponse().toArray()[0];
+        return response.isOnline();
     }
 }
