936a9e8b89a30ae0cc6aa39882527c8cb8cdda27
Denys Burlakov <burlakovden@gmail.com>
2017-04-10 22:03:28 +0300

    #6

--- src/main/java/com/big_brother/services/SpyService.java
+++ src/main/java/com/big_brother/services/SpyService.java
@@ -1,11 +1,13 @@
 package com.big_brother.services;
 
+import com.big_brother.dao.GenericDAO;
 import com.big_brother.models.UserSpied;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.config.ScheduledTask;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import javax.annotation.PostConstruct;
 import java.util.Timer;
 import java.util.TimerTask;
 
@@ -18,8 +20,21 @@ public class SpyService {
     @Autowired
     ScheduledSpyTask spyTask;
 
+    @Autowired
+    GenericDAO dao;
+
+    @PostConstruct
+    public void setupSpying(){
+        dao.getAll(UserSpied.class).forEach(this::startSpying);
+    }
+
     @Transactional
     public void spy(UserSpied userSpied){
+        dao.save(userSpied);
+        startSpying(userSpied);
+    }
+
+    private void startSpying(UserSpied userSpied) {
         Timer time = new Timer();
         spyTask.setUserSpied(userSpied);
         time.schedule(spyTask, 0, userSpied.getPeriodicity());
